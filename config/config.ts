d08a5cad1929ac67f049b11db23ca90cb35af64c
十八 <mengjiao3021@gmail.com>
2024-06-19 20:34:07 +0800

    feat(i18n): 设置默认语言为zh-CN并调整配置项，移除OpenAPI

--- config/config.ts
+++ config/config.ts
@@ -95,10 +95,8 @@ export default defineConfig({
    * @doc https://umijs.org/docs/max/i18n
    */
   locale: {
-    // default zh-CN
     default: 'zh-CN',
     antd: true,
-    // default true, when it is true, will use `navigator.language` overwrite default
     baseNavigator: true,
   },
   /**
