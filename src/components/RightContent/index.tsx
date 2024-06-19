677c3ef9a67110f69bbde1e5464ed5f6daa20a01
十八 <mengjiao3021@gmail.com>
2024-06-19 20:25:31 +0800

    refactor: 移除头部的Question组件

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -1,6 +1,4 @@
-import { QuestionCircleOutlined } from '@ant-design/icons';
 import { SelectLang as UmiSelectLang } from '@umijs/max';
-import React from 'react';
 
 export type SiderTheme = 'light' | 'dark';
 
@@ -11,21 +9,5 @@ export const SelectLang = () => {
         padding: 4,
       }}
     />
-  );
-};
-
-export const Question = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        height: 26,
-      }}
-      onClick={() => {
-        window.open('https://pro.ant.design/docs/getting-started');
-      }}
-    >
-      <QuestionCircleOutlined />
-    </div>
   );
 };
--- src/components/index.ts
+++ src/components/index.ts
@@ -6,7 +6,7 @@
  * 布局组件
  */
 import Footer from './Footer';
-import { Question, SelectLang } from './RightContent';
+import { SelectLang } from './RightContent';
 import { AvatarDropdown, AvatarName } from './RightContent/AvatarDropdown';
 
-export { Footer, Question, SelectLang, AvatarDropdown, AvatarName };
+export { AvatarDropdown, AvatarName, Footer, SelectLang };
