a80cf3b1248272af6e90dec91e0fcf4283eab338
十八 <mengjiao3021@gmail.com>
2024-06-19 20:18:05 +0800

    feat: 更新项目title，logo，ico

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -7,22 +7,19 @@ const Settings: ProLayoutProps & {
   pwa?: boolean;
   logo?: string;
 } = {
+  title: 'Frontend Template',
+  logo: 'logo.svg',
   navTheme: 'light',
-  // 拂晓蓝
-  colorPrimary: '#1890ff',
+  colorPrimary: '#1677FF',
   layout: 'mix',
   contentWidth: 'Fluid',
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
   pwa: true,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  splitMenus: false,
   iconfontUrl: '',
-  token: {
-    // 参见ts声明，demo 见文档，通过token 修改样式
-    //https://procomponents.ant.design/components/layout#%E9%80%9A%E8%BF%87-token-%E4%BF%AE%E6%94%B9%E6%A0%B7%E5%BC%8F
-  },
+  token: {},
 };
 
 export default Settings;
